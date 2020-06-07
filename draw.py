5069aaf5fd60df59f9118ebdae9e5a972076a0c4
kidwhocodes <senthilkumar.padmanabhan@gmail.com>
2020-06-07 10:46:30 -0700

    Adding Melbourne housing ML model

--- draw.py
+++ draw.py
@@ -5,11 +5,9 @@ t.goto(0,0)
 def  move_up():
     t.setheading(90)
     t.forward(20)
-def move right():
+def move_right():
     t.setheading(180)
     t.forward(20)
-def move down():
-
 
 t.listen()
 t.mainloop()
